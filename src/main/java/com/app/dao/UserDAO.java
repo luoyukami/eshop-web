f5f6d7573a3324684eb62fd1b53afd66d7c95532
xiwenAndlejian <gx673512>
2017-05-24 22:56:22 +0800

    工程demo搭建

--- src/main/java/com/app/dao/UserDAO.java
+++ src/main/java/com/app/dao/UserDAO.java
@@ -1,4 +1,14 @@
 package com.app.dao;
 
+import com.app.bean.User;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public interface UserDAO {
+
+    User getById(Integer Id);
+
+    int insert(User user);
+
+    int update(User user);
 }
--- src/main/java/com/app/dao/impl/UserDAOImpl.java
+++ src/main/java/com/app/dao/impl/UserDAOImpl.java
@@ -1,6 +1,23 @@
 package com.app.dao.impl;
 
+import com.app.bean.User;
 import com.app.dao.UserDAO;
+import org.springframework.stereotype.Repository;
 
+@Repository("userDAO")
 public class UserDAOImpl implements UserDAO {
+
+    private String SQLMAP_SPACE = "USER.";
+
+    public User getById(Integer Id) {
+        return null;
+    }
+
+    public int insert(User user) {
+        return 0;
+    }
+
+    public int update(User user) {
+        return 0;
+    }
 }
